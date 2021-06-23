af1d5568944ac13c2400ce896e9eb1bd01f3ca32
Cade Young <82617358+C19130@users.noreply.github.com>
2021-06-23 22:40:25 +0000

    made question generator print requested amount of rounds

--- question_generator_v2.py
+++ question_generator_v2.py
@@ -1,8 +1,13 @@
 import random
 
+rounds = input("How rounds would you like to play")
+rounds = int(rounds)
+
 questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-question_1 = random.choice(questions)
-print(question_1)
-questions.remove(question_1)
-print(questions)
+
+for item in range(rounds):
+  question_1 = random.choice(questions)
+  print(question_1)
+  questions.remove(question_1)
+  print(questions)
